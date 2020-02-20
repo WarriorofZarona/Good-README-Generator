7d8c38824b5204b4f89c41409964e69279afd98a
Andrew Bautista <andrewbautista85@gmail.com>
2020-02-20 17:44:30 -0500

    did npm install with new files

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const questions = [
+    "What is your Github username?",
+    "What is your Project title?",
+    "What is your project description?",
+    "What are the Table of contents?",
+
 
 ];
 
@@ -6,6 +11,16 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
+
+    var inquirer = require('inquirer');
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+        ])
+        .then(answers => {
+            // Use user feedback for... whatever!!
+        });
+
 
 }
 
